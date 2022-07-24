c120fb14c9e069fa81fb131af0b8a9cd37bdc739
valikpalica <palitsavalentin@gmail.com>
2022-07-24 19:01:53 +0300

    asd

--- route/upload.js
+++ route/upload.js
@@ -4,9 +4,12 @@ let destination = paht.join(__dirname,'../','image');
 
 const storage = multer.diskStorage({
     destination: (req,file,cb)=>{
+        console.log(`destination`);
+        console.log(destination);
         cb(null,destination);
     },
     filename: (req,file,cb)=>{
+        console.log(file);
         //let name = Date.now().toString().replace(/:/g,'-')+file.originalname;
         let name = file.originalname;
         cb(null,name);
